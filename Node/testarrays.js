f31663a40c4e6b6a6a5c6dd382edf1cf710d285a
Zyam <zyammaqsood7@gmail.com>
2022-06-18 18:33:11 +0500

    Update testarrays.js

--- Node/testarrays.js
+++ Node/testarrays.js
@@ -1,5 +1,17 @@
 console.clear()
-const name = ["Zyam", "Arsam", "Zyam2"];
+let name = ["Zyam", "Arsam", "Zyam2"];
 name.splice(2,1, "Ayesha", "Fatima")
 name.sort()
 console.log(name);
+
+// //let searchName = name.find(FindZyam("nme"));
+// const FindZyam = (nme) => nme == "Zyam" ? True : False;
+// console.log(searchName);
+
+ let nameSearch = name.find(nameFind)
+ console.log (nameSearch)
+ function nameFind (nme) {
+   
+     if (nme=="Zyam") return true;
+     else return false;
+ }
